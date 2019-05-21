90d59addc86fdf5a53547ec48ec4c74b91ce970d
Tyler Hancock <darklime@live.ca>
2019-05-21 17:02:36 -0600

    Added a general inventory check.

--- src/main/java/net/darkhax/bingo/BingoEventHandler.java
+++ src/main/java/net/darkhax/bingo/BingoEventHandler.java
@@ -1,11 +1,14 @@
 package net.darkhax.bingo;
 
 import net.darkhax.bingo.network.PacketSyncGameState;
+import net.darkhax.bookshelf.util.PlayerUtils;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent.ItemPickupEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 @EventBusSubscriber(modid = "bingo")
 public class BingoEventHandler {
@@ -26,6 +29,22 @@ public class BingoEventHandler {
         if (event.player instanceof EntityPlayerMP) {
             
             BingoMod.NETWORK.sendTo(new PacketSyncGameState(BingoMod.GAME_STATE.write()), (EntityPlayerMP) event.player);
+        }
+    }
+    
+    @SubscribeEvent
+    public static void onPlayerTick (TickEvent.PlayerTickEvent event) {
+        
+        // Only check once a second
+        if (event.player.ticksExisted % 20 == 0 && event.player instanceof EntityPlayerMP) {
+            
+            for (final ItemStack stack : event.player.inventory.mainInventory) {
+                
+                if (!stack.isEmpty()) {
+                    
+                    BingoMod.GAME_STATE.onPlayerPickupItem((EntityPlayerMP) event.player, stack);
+                }
+            }
         }
     }
 }
