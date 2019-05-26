2e264e50f3a133af48ae50b437ed46546c95a8f2
Tyler Hancock <darklime@live.ca>
2019-05-25 23:47:39 -0600

    Bingo start command will now teleport the player around.

--- src/main/java/net/darkhax/bingo/commands/CommandBingoStart.java
+++ src/main/java/net/darkhax/bingo/commands/CommandBingoStart.java
@@ -3,9 +3,16 @@ package net.darkhax.bingo.commands;
 import net.darkhax.bingo.BingoMod;
 import net.darkhax.bingo.network.PacketSyncGameState;
 import net.darkhax.bookshelf.command.Command;
+import net.darkhax.bookshelf.util.MathsUtils;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.init.MobEffects;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentTranslation;
 
 public class CommandBingoStart extends Command {
@@ -38,5 +45,35 @@ public class CommandBingoStart extends Command {
         BingoMod.GAME_STATE.start();
         server.getPlayerList().sendMessage(new TextComponentTranslation("command.bingo.start.started", sender.getDisplayName()));
         BingoMod.NETWORK.sendToAll(new PacketSyncGameState(BingoMod.GAME_STATE.write()));
+
+        for (final EntityPlayerMP player : server.getPlayerList().getPlayers()) {
+
+            this.movePlayer(player);
+        }
+    }
+
+    private void movePlayer (EntityPlayer player) {
+
+        final int x = MathsUtils.nextIntInclusive(-29_999_900, 29_999_900);
+        final int y = MathsUtils.nextIntInclusive(-29_999_900, 29_999_900);
+
+        final BlockPos.MutableBlockPos groundPos = new BlockPos.MutableBlockPos(x, 255, y);
+        player.setPositionAndUpdate(x, 255, y);
+        player.addPotionEffect(new PotionEffect(MobEffects.RESISTANCE, 240, 5));
+
+        while (player.world.isAirBlock(groundPos)) {
+
+            groundPos.move(EnumFacing.DOWN);
+
+            if (groundPos.getY() < 1) {
+
+                this.movePlayer(player);
+                return;
+            }
+        }
+
+        groundPos.move(EnumFacing.UP);
+        player.setSpawnChunk(groundPos.up(), true, player.dimension);
+        player.setPositionAndUpdate(groundPos.getX() + 0.5, groundPos.getY() + 0.5, groundPos.getZ() + 0.5);
     }
 }
